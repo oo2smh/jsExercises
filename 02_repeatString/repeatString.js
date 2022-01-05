413a4d87315e0b8a4da457b18fddd4247cc1524f
hamin <haminson@gmail.com>
2022-01-05 16:16:42 -0600

    Finish reverseString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -19,3 +19,4 @@ const repeatString = function(string, num) {
 
 // Do not edit below this line
 module.exports = repeatString;
+  
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
 const reverseString = function(str) {
+  let arr = Array.from(str);
+  arr.reverse();
+  return arr.join('');
+
+  // poa: loop backwards and input it into a new array
+  // poa: convert the array into a string without any separators join('');
 
 };
 
