f4d5d21c253ce8997f1658a2ef25b8f75ad911f2
hamin <haminson@gmail.com>
2022-01-05 15:53:24 -0600

    Finish 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+//repeat string num amount of times; sounds like a loop
 
+const repeatString = function(string, num) {
+
+  let i = 1;
+  let arr = [];
+
+  if(num < 0) {
+    return 'ERROR'
+  }
+
+  while (i <= num) {
+    arr.push(string)
+    i++;
+  } 
+
+  return arr.join('');
 };
 
 // Do not edit below this line
